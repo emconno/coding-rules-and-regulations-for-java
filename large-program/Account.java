e2c394ff744e12c731760721df8f02cc494e5f34
MichaelRosenwinkel2003 <merosenwinkel@gmail.com>
2025-03-05 00:00:37 -0600

    Add implementations for OBJ05-J: Do not return references to private mutable class members and OBJ04-J: Provide mutable classes with copy functionality to safely allow passing instances to untrusted code

--- large-program/Account.java
+++ large-program/Account.java
@@ -3,9 +3,23 @@ public class Account {
 
     //OBJ01-J: Limit accessibility of fields
     private double balance = 0; 
+    private StringBuilder fname; 
+    private StringBuilder lname; 
 
-    public Account(double balance){
+    public Account(double balance, String fname, String lname){
         this.balance = balance; 
+        this.fname = new StringBuilder(fname);
+        this.lname = new StringBuilder(lname);
+    }
+
+    //OBJ04-J: Provide mutable classes with copy functionality to safely allow passing instances to untrusted code
+    public Account(Account acct){
+
+        Account.Transaction transaction = acct.new Transaction();
+        this.balance = transaction.checkBalance();
+
+        this.fname = new StringBuilder(acct.getFName().toString());
+        this.lname = new StringBuilder(acct.getLName().toString());
     }
 
 
@@ -40,8 +54,14 @@ public class Account {
         }
     }
 
-    
+    //OBJ05-J: Do not return references to private mutable class members
+    public StringBuilder getFName(){
+        return new StringBuilder(this.fname);
+    }
 
+    public StringBuilder getLName(){
+        return new StringBuilder(this.lname);
+    }
 
     
 
