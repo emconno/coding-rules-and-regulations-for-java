4decbad38eba132d72c28c23f521920a1ac2a445
MichaelRosenwinkel2003 <merosenwinkel@gmail.com>
2025-03-04 22:56:21 -0600

    Write Primitive Account.java with basic fields and methods

--- large-program/Account.java
+++ large-program/Account.java
@@ -1,4 +1,34 @@
 
 public class Account {
-    
+
+    //limit accessibility of fields
+    private double balance = 0; 
+    private String fname; 
+    private String lname; 
+
+    public Account(String fnameInit, String lnameInit){
+        this.fname = fnameInit; 
+        this.lname = lnameInit;
+    }
+
+    public boolean withdrawal(double amount){
+        balance -= amount; 
+
+        if(balance < 0){
+            return false; 
+        }
+        return true;
+    }
+
+    public boolean deposit(double amount){
+        balance += amount; 
+
+        if(balance < 0){
+            return false; 
+        }
+
+        return true;
+    }
+
+
 }
